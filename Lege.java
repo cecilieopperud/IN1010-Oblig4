29d9d7b0fefaa2efccf6ccd5838eea6bfdbe7229
cecilieopperud <62059799+cecilieopperud@users.noreply.github.com>
2020-03-11 14:09:01 +0100

    Update Lege.java

--- Lege.java
+++ Lege.java
@@ -1,5 +1,6 @@
 public class Lege implements Comparable <Lege> { //Oppretter klassen Lege.
   protected String legeNavn; //protected variabel legeNavn slik at den er tilgjengelig for subklasser.
+  protected Lenkeliste<Resepter> utskrevedeResepter = new Lenkeliste<Resepter>();
 
   public Lege(String navn) { //Konstruktoer som tar imot en String navn.
     legeNavn = navn; //settes som verdien til legeNavn.
@@ -12,4 +13,11 @@ public class Lege implements Comparable <Lege> { //Oppretter klassen Lege.
   public String toString() { //Metode for aa returnere en string til utskrift.
     return legeNavn;
   }
+  
+  @Override 
+  public String compareTo(Lege lege){
+    return lege.hentLegeNavn().compareTo(legeNavn.hentLegeNavn());
+  }
+  public Resepter hentUtResepter(){
+    return utskrevedeResepter.hentListe();
 }
