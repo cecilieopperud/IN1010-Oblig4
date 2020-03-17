f701ceb93839aed5fa4fd5894cd22cdbf01914c2
cecilieopperud <62059799+cecilieopperud@users.noreply.github.com>
2020-03-17 16:01:21 +0100

    Delete Vanlig.java

--- Vanlig.java
+++
@@ -1,6 +0,0 @@
-public class Vanlig extends Legemiddel { //subklasse av Legemiddel.
-
-  public Vanlig(String navn, double pris, double stoff) {
-    super(navn, pris, stoff); //aksesserer superklassens konstruktoer.
-  }
-}
